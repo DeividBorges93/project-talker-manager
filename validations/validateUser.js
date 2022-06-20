bb3000123d897670553ba873b452787bb4337e44
DeividBorges93 <deividmborges@gmail.com>
2022-06-20 18:21:30 -0300

    feat: validateUser

---
+++ validations/validateUser.js
@@ -0,0 +1,26 @@
+const validateEmail = (req, res, next) => {
+  const regexEmail = ('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!email.match(regexEmail)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  next();
+};
+
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+
+  next();
+};
+
+module.exports = { validateEmail, validatePassword };
