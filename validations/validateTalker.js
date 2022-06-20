37d9c53cf772a5725ac274252f45bba60cf337dd
DeividBorges93 <deividmborges@gmail.com>
2022-06-20 18:51:18 -0300

    feat: validateToken

---
+++ validations/validateTalker.js
@@ -0,0 +1,8 @@
+const validateToken = (req, res, next) => {
+  const { authorization: token } = req.headers;
+  if (!token) return res.status(401).json({ message: 'Token não encontrado' });
+  if (token.length !== 16) return res.status(401).json({ message: 'Token inválido' });
+
+  next();
+};
+
