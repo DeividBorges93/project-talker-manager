22429990f3c8c724982ad9d47450f2699cb1b4a5
DeividBorges93 <deividmborges@gmail.com>
2022-06-22 15:08:01 -0300

    feat: middlewareError

---
+++ middlewares/middlewareError.js
@@ -0,0 +1,8 @@
+const middlewareError = (err, _req, res, _next) => {
+  if (err.status) return res.status(err.status).json({ message: err.message });
+  return res.status(500).json(err.message);
+};
+
+module.exports = {
+  middlewareError,
+};
