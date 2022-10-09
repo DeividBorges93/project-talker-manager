b9ae310e9bc1b3c1b0ef1837e407f77583bb1e3b
DeividBorges93 <deividmborges@gmail.com>
2022-10-09 01:31:46 -0300

    fix(routes): :bug: send index of routes

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const loginRoutes = require('./Login.routes');
+const talkerRoutes = require('./Talker.routes');
+const docAPIRoute = require('./Swagger.routes');
+
+module.exports = {
+  loginRoutes,
+  talkerRoutes,
+  docAPIRoute
+};
