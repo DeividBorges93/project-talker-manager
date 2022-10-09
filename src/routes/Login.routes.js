820b16ebb1be8b8c295165eca74de89de8e38aff
DeividBorges93 <deividmborges@gmail.com>
2022-10-09 01:25:32 -0300

    refactor(routes): :recycle: remove index.js routes and create own folder
    LoginRoutes, TalkerRoutes
    
    SwaggerRoute: route to api documentation

---
+++ src/routes/Login.routes.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const router = Router();
+
+const { createLogin } = require('../../middlewares/createLogin');
+const {
+  validateEmail,
+  validatePassword } = require('../../validations/validateUser');
+
+router.post('/login', validateEmail, validatePassword, createLogin);
+
+module.exports = router;
